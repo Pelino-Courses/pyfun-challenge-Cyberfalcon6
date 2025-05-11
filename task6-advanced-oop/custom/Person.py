84d36293d3c3d6511128ecfbb43f9baf126e8d83
CyberFalcon <cyberfalcon660066@gmail.com>
2025-05-11 19:09:02 +0300

    Clearing the directory

--- task6-advanced-oop/custom/Person.py
+++
@@ -1,8 +0,0 @@
-class Person:
-    """This is the class that contains attributes of all person objec
-    that we will have in our university system. some other classes will inherit it.
-    """
-    def __init__(self, person_name, person_age, person_sex):
-        self.person_name = person_name
-        self.person_age = person_age
-        self.person_sex = person_sex
--- task6-advanced-oop/custom/student.py
+++
@@ -1,5 +0,0 @@
-from Person import Person
-class Student(Person):
-    def __init__(self, student_name, student_age, student_sex):
-        super.__init__(student_name, student_age, student_sex)
-        
