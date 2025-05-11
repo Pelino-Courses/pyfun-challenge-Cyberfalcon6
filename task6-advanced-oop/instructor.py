d189f3e5caa4e756b5ff03c09f2fa6886c52a251
CyberFalcon <cyberfalcon660066@gmail.com>
2025-05-11 18:47:33 +0300

    refining University management system

--- task6-advanced-oop/instructor.py
+++ task6-advanced-oop/instructor.py
@@ -1,15 +1,15 @@
 from person import Person
 
 class Instructor(Person):
-    def __init__(self, name: str, age: int, employee_id: str):
+    def __init__(self, name: str, age: int, instructor_id: str):
         super().__init__(name, age)
-        if not isinstance(employee_id, str) or not employee_id.strip():
+        if not isinstance(instructor_id, str) or not instructor_id.strip():
             raise ValueError("Employee ID must be a non-empty string.")
-        self.employee_id = employee_id
+        self.instructor_id = instructor_id
         self.courses_taught = []
 
     def assign_course(self, course):
         self.courses_taught.append(course)
 
     def __str__(self):
-        return f"{super().__str__()}, Employee ID: {self.employee_id}, Courses Taught: {[course.name for course in self.courses_taught]}"
+        return f"{super().__str__()}, Instructor ID: {self.instructor_id}, Courses Taught: {[course.name for course in self.courses_taught]}"
--- task6-advanced-oop/university_demo.py
+++ task6-advanced-oop/university_demo.py
@@ -1,32 +1,160 @@
 from student import Student
 from instructor import Instructor
+from random import randint
 from course import Course
 from enrollment import Enrollment
 from teaching_assistant import TeachingAssistant
 
-def main():
-    student1 = Student("Alice", 20, "S001")
-    student2 = Student("Bob", 22, "S002")
-    instructor = Instructor("Dr. Smith", 45, "E001")
-    course1 = Course("Math 101", "M101")
-    course2 = Course("Physics 101", "P101")
+class University():
 
-    Enrollment(student1, course1)
-    Enrollment(student2, course2)
+    def __init__(self):
+        self.students:list[Student] = [Student("Japhet Niyogakiza", 23, "224002651")]
+        self.courses:list[Course] = [Course("Mathematics", "101")]
+        self.instructors:list[Instructor] = [Instructor("Karamage J.Claude", 24, str(randint(10000, 99999)))]
+        self.enrollments:list[Enrollment] = []
+        self.tas:list[TeachingAssistant] = []
+        print("Welcome to University Management System")
+        print("==========================================")
+        print("""
+                1. View University Elements 
+                2. View Students
+                3. View Courses
+                4. View Instructors
+                5. View Teaching assistants
+                6. Add student
+                7. Add course
+                8. Add Instructor
+                9. View enrollments
+                10. Enroll a student
+                11. Assign Course
+                0. Exit
+            >>>>| """, end="")
+        choice = int(input())
+        while choice!=0:
+            if choice==1:
+                print("""
+                    In university management System there are \n
+                    - Students
+                    - Instructors
+                    - Courses 
+                    - Teaching Assistants
+                    """)
+            elif choice==2:
+                for student in self.students:
+                    print(student)
+            elif choice == 3:
+                for course in self.courses:
+                    print(course)
+            elif choice == 4:
+                for instructor in self.instructors:
+                    print(instructor)
+            elif choice == 5:
+                for ta in self.tas:
+                    print(ta)
+            elif choice == 6:
+                student_name = input("Student Name: ")
+                student_age = int(input("Student age: "))
+                student_id = str(randint(10000, 99999))
+                temp_student = Student(student_name, student_age, student_id=student_id)
+                self.students.append(temp_student)
+            elif choice == 7:
+                course_name = input("Course Name: ")
+                course_code = input("Course code: ")
+                temp_course = Course(course_name, course_code)
+                self.courses.append(temp_course)
+            elif choice == 8:
+                instructor_name = input('Instructor Name: ')
+                instructor_age = int(input("Instructor age: "))
+                instructor_id = str(randint(10000, 99999))
+                temp_instructor = Instructor(instructor_name, instructor_age, instructor_id)
+                self.instructors.append(temp_instructor)
+            elif choice == 9:
+                for enrollment in self.enrollments:
+                    print(enrollment)
+            elif choice == 10:
+                for student in self.students:
+                    print(student)
+                student_id = input("Student ID you want to enroll: ")
+                student = self.getStudentById(student_id)
+                for course in self.courses:
+                    print(course)
+                course_id = input("Course ID of Enrollment: ")
+                course = self.getCourseById(course_id)
+                temp_enrollment = Enrollment(student, course)
+                self.enrollments.append(temp_enrollment)
+            elif choice == 11:
+                for instructor in self.instructors:
+                    print(instructor)
+                instructor_id = input("Enter instructor id: ")
+                instructor = self.getInstructorById(instructor_id)
+                for course in self.courses:
+                    print(course)
+                course_id = input("Enter the course id to assign: ")
+                course = self.getCourseById(course_id)
+                instructor.assign_course(course)
+            else:
+                print("Please enter choice between 1 and 5")
+            print("""
+                1. View University Elements 
+                2. View Students
+                3. View Courses
+                4. View Instructors
+                5. View Teaching assistants
+                6. Add student
+                7. Add course
+                8. Add Instructor
+                9. View enrollments
+                10. Enroll a student
+                11. Assign Course
+                0. Exit
+            >>>>| """, end="")
+            choice = int(input())
 
-    instructor.assign_course(course1)
-    instructor.assign_course(course2)
+        # student1 = Student("Alice", 20, "S001")
+        # student2 = Student("Rumaga", 22, "S002")
+        # instructor = Instructor("Dr. Smith", 45, "E001")
+        # course1 = Course("Math 101", "M101")
+        # course2 = Course("Physics 101", "P101")
 
-    ta = TeachingAssistant("Charlie", 25, "S003", "E002")
-    ta.enroll(course1)
-    ta.assign_course(course2)
+        # Enrollment(student1, course1)
+        # Enrollment(student2, course2)
 
-    print(student1)
-    print(student2)
-    print(instructor)
-    print(course1)
-    print(course2)
-    print(ta)
+        # instructor.assign_course(course1)
+        # instructor.assign_course(course2)
 
+        # ta = TeachingAssistant("Kalisa", 25, "S003", "E002")
+        # ta.enroll(course1)
+        # ta.assign_course(course2)
+
+        # print(student1)
+        # print(student2)
+        # print(instructor)
+        # print(course1)
+        # print(course2)
+        # print(ta)
+    def getStudentById(self, student_id):
+        """
+        Returns Student object matching the id passed to it
+        """
+        for student in self.students:
+            if student_id == student.student_id:
+                return student
+        return None
+    def getInstructorById(self, instructor_id):
+        """
+        Returns Instructor object matching the id passed to it
+        """
+        for instructor in self.instructors:
+            if instructor_id == instructor.instructor_id:
+                return instructor
+        return None
+    def getCourseById(self, course_code):
+        """
+        Returns Course object matching the id passed to it
+        """
+        for course in self.courses:
+            if course_code == course.code:
+                return course
+        return None
 if __name__ == "__main__":
-    main()
+    Uni = University()
